b630f7a536fba3202b58568397f60a8a9f157bbb
Janne Valkealahti <janne.valkealahti@gmail.com>
2019-10-31 19:46:23 +0000

    stash

--- ci/scripts/vscode-extension-di-publish.sh
+++ ci/scripts/vscode-extension-di-publish.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 set -e
-cd git-repo
 
+buildversion=`date '+%Y-%m-%d-%H-%M-%S'`
+
+pushd git-repo > /dev/null
 envsubst < ~/.npmrc > ~/.npmrc.tmp && mv  ~/.npmrc.tmp ~/.npmrc
 npm install && npm pack && npm run publish:next
+popd > /dev/null
+
+pushd triggers > /dev/null
+mkdir ${BUILD_NAME}
+touch ${BUILD_NAME}/trigger-${buildversion}
+popd > /dev/null
